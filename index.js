2840a67f112f3334a8f87ef8015690be2f0e8252
nurersoyozyurek <nersoy88@gmail.com>
2021-06-12 12:50:23 +0200

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+
+'use strict';
+var AWS = require('aws-sdk'),
+    //uuid =  require('uuid'),
+    myDocumentClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async function (event, context) {
+    var params = {
+        Item: {
+            "user_id": event.user_id, //uuid.v1(),
+            "user_name": event.user_name
+        },
+        TableName: process.env.USER_TABLE_NAME
+    };
+    myDocumentClient.put(params, function (err, data) {
+       // callback(err, data);
+    });
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(event.user_id + " id inserted into table named " + process.env.USER_TABLE_NAME),
+    };
+    return response;
+
+};
